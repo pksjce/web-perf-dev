a62137d895ce0e2d7e7e530ca188a816dec8a4a5
Pavithra Kodmad <pksjce@gmail.com>
2021-08-24 21:10:13 +1000

    First version of web performance landing page

--- src/_data/meta.js
+++ src/_data/meta.js
@@ -1,8 +1,8 @@
 module.exports = {
   url: process.env.URL || "http://localhost:8080",
-  siteName: "11ty Netlify Jumpstart",
+  siteName: "Web Performance workshops",
   siteDescription:
-    "Quickly launch an 11ty-generated static site. Includes a minimal Sass framework, and generated sitemap, RSS feed, and social share preview images.",
-  authorName: "Stephanie Eckles",
-  twitterUsername: "5t3ph", // no `@`
+    "Unique hands on workshops to learn how to measure, mitigate and maintain client side performance for web pages and apps.",
+  authorName: "Pavithra Kodmad",
+  twitterUsername: "pkodmad", // no `@`
 };
